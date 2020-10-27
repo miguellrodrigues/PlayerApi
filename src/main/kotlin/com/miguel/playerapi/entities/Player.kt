e43ebfe9bca013c6f41de82aa20d4758232665ce
Miguel <miguellukas52@gmail.com>
2020-10-27 18:23:05 -0300

    add kills deaths and wins to player`s document

--- src/main/kotlin/com/miguel/playerapi/entities/Player.kt
+++ src/main/kotlin/com/miguel/playerapi/entities/Player.kt
@@ -7,6 +7,9 @@ import java.util.*
 @Document
 class Player(
         @Id
-        var id: String? = UUID.randomUUID().toString(),
-        var name: String
-) {}
+        var id: String = UUID.randomUUID().toString(),
+        var name: String,
+        var kills: Int,
+        var deaths: Int,
+        var wins: Int
+)
